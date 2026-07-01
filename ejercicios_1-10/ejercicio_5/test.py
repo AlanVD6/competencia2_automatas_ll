2b995ffe601e55055dceb1e8c1e04fbf5686b47c
AlanVD6 <ghostdiaz616@gmail.com>
2026-07-01 17:48:03 -0600

    Ejercicicio 2 con archivo N_cntrl 23030022

--- ejercicios_1-10/ejercicio_5/test.py
+++ ejercicios_1-10/ejercicio_5/test.py
@@ -1,19 +1,19 @@
 #Ejercicio 5
 # Numero de control 23030022
 
-from antlr4 import*
+from antlr4 import *
 from ExprLexer import ExprLexer
 
-lexer = ExprLexer (InputStream(input("? ")))
+lexer = ExprLexer(InputStream(input("? ")))
 tokens = CommonTokenStream(lexer)
 tokens.fill()
-print (tokens)
+print(tokens)
 
 for token in tokens.tokens:
-    print ("Texto : ",token.text)
-    print ("linea : ",token.line)
-    print ("columna : ",token.column)
+    print("Texto : ", token.text)
+    print("linea : ", token.line)
+    print("columna : ", token.column)
     nombre_token = lexer.symbolicNames[token.type]
-    print ("tipo : ",nombre_token)
-    print ("----------------------")
+    print("tipo : ", nombre_token)
+    print("----------------------")
 
